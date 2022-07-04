741a33716f33602241fdfed1e680dafa7f7a5ed7
takami-ml <91838137+takami-ml@users.noreply.github.com>
2022-07-04 16:44:43 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+import time
+from logging import getLogger
+
+from src.arbitrager import Arbitrager, ArbitragerConfig
+from src.contracts.utils import get_w3
+from src.exchanges.ftx import (FtxOrderer, FtxPositionGetter,
+                               FtxPositionGetterConfig, FtxRestTicker,
+                               FtxRestTickerConfig)
+from src.exchanges.perpdex import (PerpdexOrderer, PerpdexPositionGetter,
+                                   PerpdexPositionGetterConfig, PerpdexTicker,
+                                   PerpdexTickerConfig)
+from src.spread_calculator import TakeTakeSpreadCalculator
+from src.target_pos_calculator import (TargetPosCalculator,
+                                       TargetPosCalculatorConfig)
+from src.trade_executer import TargetTakePositioner, TargetTakePositionerConfig
+
+
+# resolve dependencies
+def _init_ftx_perpdex_arb_bot() -> Arbitrager:
+    _ftx_symbol = 'BTC/USD'
+    _perpdex_symbol = 'BTC'
+
+    _ftx_position_getter = FtxPositionGetter(
+        config=FtxPositionGetterConfig(symbol=_ftx_symbol),
+    )
+
+    _w3 = get_w3(
+        network_name=os.environ['WEB3_NETWORK_NAME'],
+        web3_provider_uri=os.environ['WEB3_PROVIDER_URI'],
+        user_private_key=os.environ['USER_PRIVATE_KEY'],
+    )
+
+    # 1: ftx, 2: perpdex
+    target_pos_calculator = TargetPosCalculator(
+        spread_getter=TakeTakeSpreadCalculator(
+            price_getter1=FtxRestTicker(
+                config=FtxRestTickerConfig(symbol=_ftx_symbol),
+            ),
+            price_getter2=PerpdexTicker(
+                w3=_w3,
+                config=PerpdexTickerConfig(symbol=_perpdex_symbol),
+            ),
+        ),
+        position_getter=_ftx_position_getter,
+        config=TargetPosCalculatorConfig(),
+    )
+
+    # 1: ftx, 2: perpdex
+    positioner1 = TargetTakePositioner(
+        position_getter=_ftx_position_getter,
+        taker=FtxOrderer(),
+        config=TargetTakePositionerConfig(symbol=_ftx_symbol),
+    )
+
+    positioner2 = TargetTakePositioner(
+        position_getter=PerpdexPositionGetter(
+            w3=_w3,
+            config=PerpdexPositionGetterConfig(symbol=_perpdex_symbol),
+        ),
+        taker=PerpdexOrderer(w3=_w3),
+        config=TargetTakePositionerConfig(symbol=_perpdex_symbol),
+    )
+
+    return Arbitrager(
+        target_pos_calculator=target_pos_calculator,
+        positioner1=positioner1,
+        positioner2=positioner2,
+        config=ArbitragerConfig(trade_loop_sec=1),
+    )
+
+
+def main():
+    logger = getLogger(__name__)
+    while True:
+        arb = _init_ftx_perpdex_arb_bot()
+        arb.start()
+        while arb.health_check():
+            time.sleep(30)
+        logger.warning('Restarting Arb bot')
