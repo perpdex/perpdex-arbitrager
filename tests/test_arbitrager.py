7c73eab3015c99664a95d4c98425b399e30d9420
takami-ml <91838137+takami-ml@users.noreply.github.com>
2022-07-23 14:55:45 +0900

    Add rebalancer to arbitrager

--- tests/test_arbitrager.py
+++ tests/test_arbitrager.py
@@ -21,11 +21,17 @@ async def test_arbitrager(mocker):
     future2.set_result(None)
     mocked_chaser2.execute.return_value = future2
 
+    mocked_rebalancer = mocker.MagicMock()
+    future3 = asyncio.Future()
+    future3.set_result(None)
+    mocked_rebalancer.rebalance.return_value = future3
+ 
     arb = Arbitrager(
         target_pos_calculator=mocked_target_pos_calculator,
         position_chaser1=mocked_chaser1,
         position_chaser2=mocked_chaser2,
-        config=ArbitragerConfig(trade_loop_sec=1, inverse=False),
+        liquidity_rebalancer=mocked_rebalancer,
+        config=ArbitragerConfig(trade_loop_sec=1, rebalance_loop_sec=1, inverse=False),
     )
 
     # assert health check ok
@@ -35,10 +41,25 @@ async def test_arbitrager(mocker):
     # wait 1 sec to make sure trade loop executed
     await asyncio.sleep(1)
 
-    # force revert the running task
-    arb._task.cancel()
+    # force revert the running _trade task
+    arb._task_t.cancel()
     try:
-        await arb._task
+        await arb._task_t
+    except asyncio.CancelledError:
+        pass
+
+    # assert health check ng
+    assert arb.health_check() is False
+
+    # restart arb
+    arb.start()
+    assert arb.health_check() is True
+    await asyncio.sleep(1)
+
+    # force revert the running _rebalace task
+    arb._task_m.cancel()
+    try:
+        await arb._task_m
     except asyncio.CancelledError:
         pass
 
